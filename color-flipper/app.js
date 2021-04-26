a982ab1e443a213b84cee2c15d13d45c758ec192
astratow <davno_pora@mail.com>
2021-04-26 05:26:16 +0100

    smilga color flipper updated: some fixes and comments

--- color-flipper/app.js
+++ color-flipper/app.js
@@ -1,14 +1,18 @@
+//This program changes body color into one from the array colors after clicking click me button. 
+//It randomizes color by using getRandomNumber() which generates random between 0 and 1
+//and multiplies it by number of array elements.
+//After that it rounds it using Math.floor()
 const colors = ["green", "red", "rgba(133, 122, 200)", "#f15025"];
 const btn = document.getElementById('btn');
 const color = document.querySelector(".color");
 
 btn.addEventListener('click', function(){
     // get random number between 0 and 3
-    const randomNumber = getRandomNmber();
+    const randomNumber = getRandomNumber();
     document.body.style.backgroundColor = colors[randomNumber];
-    color.textContent = color[randomNumber];
+    color.textContent = colors[randomNumber];
 })
 
-function getRandomNmber(){
+function getRandomNumber(){
     return Math.floor(Math.random()*colors.length);
 }
