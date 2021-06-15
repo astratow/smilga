61cc48aec1cb2a4a5ab09d88e6d33177b5950cba
astratow <davno_pora@mail.com>
2021-06-15 06:09:37 +0100

    final changes

--- navbar/app.js
+++ navbar/app.js
@@ -1,19 +1,14 @@
 // navbar project
 
-// classList - shows/gets all classes
-// contains - checks classList for specific class
-// add - add class
-// remove - remove class
-// toggle - toggles class
+
 const navToggle = document.querySelector(".nav-toggle");
 const links = document.querySelector(".links");
 
 navToggle.addEventListener('click', function(){
-	// console.log(links.classList);
-	// console.log(links.classList.contains("random"));
-	if(links.classList.contains('show-links')){
+	
+/*	if(links.classList.contains('show-links')){
 		links.classList.remove('show-links');
-	}else{
-		links.classList.add("show-links");
-	}
+	}else{*/
+		links.classList.toggle("show-links");
+	//}
 });
